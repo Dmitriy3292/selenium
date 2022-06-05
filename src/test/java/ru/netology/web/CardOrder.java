c55909ba31dfdf3fccac0709becd6de405788b2f
Dmitriy3292 <mr.diy@yandex.ru>
2022-06-05 15:30:59 +0400

    ref code

--- src/test/java/ru/netology/web/CardOrder.java
+++ src/test/java/ru/netology/web/CardOrder.java
@@ -18,41 +18,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardOrder {
 
- private WebDriver driver;
+    private WebDriver driver;
 
- @BeforeAll
- public static void setUpAll() {
+    @BeforeAll
+    public static void setUpAll() {
 
-  WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
 
 
- }
-@BeforeEach
- void setUp2(){    // запускается перед каждым тестом
- ChromeOptions options = new ChromeOptions();
- options.addArguments("--disable-dev-shm-usage");
- options.addArguments("--no-sandbox");
- options.addArguments("--headless");
- driver = new ChromeDriver(options);
-}
+    }
 
-@AfterEach     // закрывает все окна браузера
- public void close(){
- driver.quit();
- driver=null;
-}
+    @BeforeEach
+    void setUp2() {    // запускается перед каждым тестом
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
 
-@Test
-  public void test(){
- driver.get("http://localhost:9999/");
- List<WebElement> elements= driver.findElements(By.className("input__control"));
- driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
- driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79867281447");
- driver.findElement(By.className("checkbox__box")).click();
- driver.findElement(By.className("button__content")).click();
-String text = driver.findElement (By.cssSelector("[data-test-id=\"order-success\"]")).getText();
+    @AfterEach     // закрывает все окна браузера
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
 
- assertEquals ("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-}
+    @Test
+    public void test() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79867281447");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 
 }
